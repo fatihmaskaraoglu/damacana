fc7d1e9c50535400450d98992645ba702d38e5a8
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2015-11-29 20:25:14 +0200

    AdminPart2 Completed
    Add product
    save Product
    back to list done

--- damacana/Controllers/HomeController.cs
+++ damacana/Controllers/HomeController.cs
@@ -3,15 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using damacana.Models;
 
-namespace damacana.Controllers
+namespace Damacana.Controllers
 {
     public class HomeController : Controller
     {
+        //List of products in the Memory
+        public static List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Erikli 19L",
+                Price = (decimal) 4.90
+            },
+            new Product()
+            {
+                Id = 1,
+                Name = "Pinar 19L",
+                Price = (decimal)5.90
+            }
+        };
+
+
         public ActionResult Index()
         {
-            return View();
+            //Send product to the View Engine
+            return View(products);
         }
+
+        public ActionResult AddProduct()
+        {
+            //Create an Empty Product
+            Product product = new Product()
+            {
+                Name = "",
+                Price = (decimal)0
+            };
+
+            //Pass it to the View
+            return View(product);
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveProduct(Product product)
+        {
+
+            //Save to the list in Memory
+            products.Add(product);
+
+            return View(product);
+        }
+
+
+
 
         public ActionResult About()
         {
